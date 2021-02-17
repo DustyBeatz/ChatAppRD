508d24b56ad5059f0ba9eeb819cd882eea2b1d3f
Reece Devine <reece.devine97@gmail.com>
2021-02-17 11:26:56 -0330

    adding files to dev.RD.Socket

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,4 +2,21 @@
     console.log("Fired");
     //load the socket library
     const socket = io();
+
+    const vm = new VTTCue({
+       data: { message:[],
+        nickname: '',
+        username: '',
+        },
+
+        created: function() {
+            console.log("it's live");
+        },
+        
+        methods: {
+
+        },
+    }).$mount('#app');
+
+
 })();
