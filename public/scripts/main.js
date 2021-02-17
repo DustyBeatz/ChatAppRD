ecc691a154c2f0f356baa44340490de6100fdab3
Reece Devine <reece.devine97@gmail.com>
2021-02-17 16:00:29 -0330

    added the vue for message viewing

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,22 +1,53 @@
+import ChatMessage from "./components/TheMessageComponent.js";
+
 (() =>{
     console.log("Fired");
     //load the socket library
     const socket = io();
 
-    const vm = new VTTCue({
-       data: { message:[],
+    //messenger service event handling -> incoming from the manager
+
+    function setUserId(sID, message) {
+        //incoming connected event with with data
+        // debugger;
+
+        vm.socketID = sID;
+    }
+
+    function appendMessage(message) {
+
+        // debugger;
+        vm.messages.push(message); 
+    }
+
+    const vm = new Vue({
+       data: { 
+        messages: [],
         nickname: '',
         username: '',
+        socketID: '',
+        message: ''
         },
 
         created: function() {
             console.log("it's live");
         },
-        
+
         methods: {
 
+            dispatchMessage() {
+                // debugger;
+                socket.emit('chatmessage', {content: this.message, name: this.nickname || "Anonymous"})
+            }
+
         },
+
+        components: {
+            newmessage: ChatMessage
+        },
+
     }).$mount('#app');
 
-
+    socket.addEventListener("connected", setUserId);
+    socket.addEventListener('message', appendMessage);
 })();
